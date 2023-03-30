fd6d56f87b686c618a21eafe8755d720ec43b7c8
Simon Kalyakulin <kalyakulin.semen@gmail.com>
2023-03-31 01:05:21 +0700

    backup.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tools import kernels
 from tools import image_processing
 from typing import Final
-import tools
 import cv2 as cv
 
 SCALE_BY: Final = 2
@@ -20,24 +19,6 @@ def main():
     )
 
     cv.imwrite('foo/bar.jpg', processed_image)
-
-    # raw_images = image_processing.read_images(paths=IMAGE_PATHS)
-    #
-    # for processes_available in (1, 2, 4, 6, 8, 10, 12, 14, 16):
-    #     image_processing.convolve_images_multiprocess(
-    #         images=raw_images,
-    #         kernel=kernels.RELIEF,
-    #         processes=processes_available,
-    #     )
-    # print('-' * 40)
-    #
-    # for processes_available in (1, 2, 4, 6, 8, 10, 12, 14, 16):
-    #     image_processing.scale_images_multiprocess(
-    #         images=raw_images,
-    #         scale_by=SCALE_BY,
-    #         processes=processes_available,
-    #     )
-    # print('-' * 40)
 
 
 if __name__ == '__main__':
